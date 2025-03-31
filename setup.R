fc3610c6a589f3fe592d105501158ed894e2e4f0
jeff0uille <macaraegjeff@hotmail.com>
2025-03-31 18:26:56 +0200

    Ajout d'un setup.R
    Pour que ça installe les packages un poil plis vite

---
+++ setup.R
@@ -0,0 +1,26 @@
+# Set the CRAN mirror
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# load the required packages and install them if they are not.
+packages <- c(
+  "here",
+  "QRM",
+  "tseries",
+  "nortest",
+  "stats",
+  "MASS",
+  "ggplot2",
+  "fpp3",
+  "forecast",
+  "fGarch",
+  "TTR"
+)
+
+# Function that install the packages if not already installed on your computer
+for (pkg in packages) {
+  if (!pkg %in% installed.packages()) {
+    install.packages(pkg)}}
+
+# load the packages
+for (pkg in packages) {
+  library(pkg, character.only = TRUE)}
